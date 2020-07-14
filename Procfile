dc654562d039fd85856d1a20c543c8bad1bdbd39
Rajarsi Saha <rajarsi3997@gmail.com>
2020-07-14 23:42:38 +0530

    commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:socketio --log-file - --preload
+web: gunicorn wsgi:app --log-file - --preload
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from app import socketio, app
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
